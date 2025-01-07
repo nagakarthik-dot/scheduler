f8fa57eb596ef3cb4e04609fa5e4cf52ffbe6787
nagakarthik-dot <nagakarthikjetti@gmail.com>
2025-01-07 17:07:20 +0530

    done

--- Time_Scheduling/Database.cs
+++ Time_Scheduling/Database.cs
@@ -4,6 +4,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using Microsoft.Data.SqlClient;
+
 namespace Time_Scheduling
 {
     public static class Database
